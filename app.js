f0a8e19c4eeee460faf3d778a59a590381039d3f
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-03-28 00:02:47 -0500

    testing

--- app.js
+++ app.js
@@ -2,10 +2,39 @@ const express = require("express");
 const cors = require('cors');
 const bodyparser = require("body-parser");
 const connection = require("./connection");
-const { Request } = require("tedious");
-let redis = require("redis");
-let crypto = require('crypto');
+app.post('/getQuakesByMagRange', (req, res) => {
+    let magRange1 = 1; // req.body.magRange1;
+    let magRange2 = 2;; // req.body.magRange2;
 
+    let sqlQuery = `Select * from eq where mag >= ${magRange1} and mag <= ${magRange2}`;
+    getResult(sqlQuery, res);
+});
+
+function getResult(sqlQuery, res) {
+    const request = new Request(sqlQuery,
+        (err, rowCount) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(`${rowCount} row(s) returned`);
+            }
+        }
+    );
+    const rows = [];
+    request.on("row", columns => {
+        let row = {};
+        columns.forEach((column) => {
+            row[column.metadata.colName] = column.value;
+        });
+        rows.push(row);
+    });
+    
+    request.on('doneInProc', (rowCount, more) => {
+        console.log(rowCount + ' rows returned');
+        res.send(rows);
+    });
+   connection.execSql(request);
+}
 var app = express();
 app.use(bodyparser.json());
 app.use(cors({ origin: '*' }));
